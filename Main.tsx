ad8207b340f9b0fea31f9246cda91798488cdbdc
Jmestrelozano <jmestrelozano@gmail.com>
2022-12-15 18:23:16 -0500

    creando splash screen

--- Main.tsx
+++ Main.tsx
@@ -3,8 +3,13 @@ import {Provider} from 'react-redux';
 import {persistor, store} from './src/domain/store/store';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {StackNavigations} from './src/presentation/navigations/StackNavigations';
+import SplashScreen from 'react-native-splash-screen';
 
 const Main = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
